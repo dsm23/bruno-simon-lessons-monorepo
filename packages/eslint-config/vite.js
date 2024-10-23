422b8f0d3aa282046b827d252e8f171f594821ce
David Murdoch <dsmurdoch357@gmail.com>
2024-10-23 15:10:30 +0100

    chore(eslint): upgrade eslint to v9 flat config

---
+++ packages/eslint-config/vite.js
@@ -0,0 +1,75 @@
+import js from "@eslint/js";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+/** @type {import("eslint").Linter.Config} */
+export default tseslint.config(
+  {
+    ignores: [
+      "node_modules",
+      ".next",
+      ".swc",
+      ".turbo",
+      "build",
+      "coverage",
+      "dist",
+      "global.d.ts",
+      "junit.xml",
+      "storybook-static/**",
+      "test-results",
+      "playwright-report",
+      "blob-report",
+      "playwright",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  react.configs.flat["jsx-runtime"],
+  {
+    files: ["**/*.{js,md,mdx,mjs,ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      // parserOptions: {
+      //   project: ["./tsconfig.node.json", "./tsconfig.app.json"],
+      //   tsconfigRootDir: import.meta.dirname,
+      // },
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+    },
+  },
+  {
+    files: ["!**/src/**", "**/src/stories/**"],
+    ...tseslint.configs.disableTypeChecked,
+  },
+  {
+    files: ["**/src/**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    rules: {
+      "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        {
+          prefer: "type-imports",
+        },
+      ],
+      "@typescript-eslint/no-non-null-assertion": "off",
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        {
+          allowNumber: true,
+          allowBoolean: true,
+        },
+      ],
+    },
+  },
+);
