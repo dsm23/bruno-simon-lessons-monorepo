233bc069cf3c2762365c7a5d27dffbd5a83e00b3
David Murdoch <dsmurdoch357@gmail.com>
2024-10-23 16:56:39 +0100

    feat(new lesson): add basics lesson 4
    * transforming objects

--- apps/basics-lesson-3/src/main.ts
+++ apps/basics-lesson-3/src/main.ts
@@ -19,7 +19,7 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
 
-const canvas = document.querySelector("canvas.webgl");
+const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
 
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
