b40943943268b6c7f4ae71c959d6de12899c42ca
David Murdoch <dsmurdoch357@gmail.com>
2024-10-24 16:04:39 +0100

    feat(basics): add basics lesson 5
    * basics of animation, introduction of gsap

---
+++ apps/basics-lesson-5/playwright-tests/home.spec.ts
@@ -0,0 +1,26 @@
+import AxeBuilder from "@axe-core/playwright";
+import { expect, test } from "@playwright/test";
+
+test("has title", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(page).toHaveTitle("Basics - lesson 5 | Threejs Journey");
+});
+
+test("has heading", async ({ page }) => {
+  await page.goto("/");
+
+  await expect(
+    page.getByRole("heading", { name: "Basics - lesson 5" }),
+  ).toBeVisible();
+});
+
+test("should not have any automatically detectable accessibility issues", async ({
+  page,
+}) => {
+  await page.goto("/");
+
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
+});
---
+++ apps/basics-lesson-5/src/main.ts
@@ -0,0 +1,41 @@
+import gsap from "gsap";
+import * as THREE from "three";
+
+import "./index.css";
+
+const scene = new THREE.Scene();
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+const canvas = document.querySelector("canvas.webgl") as HTMLCanvasElement;
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
+
+gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
+gsap.to(mesh.position, { duration: 1, delay: 3, x: 0 });
+
+const tick = () => {
+  renderer.render(scene, camera);
+
+  window.requestAnimationFrame(tick);
+};
+
+tick();
