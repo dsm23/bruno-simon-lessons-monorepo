1f8cc43a6e3e584bc4775777cf5544033cc34b13
David Murdoch <dsmurdoch357@gmail.com>
2024-11-12 16:28:01 +0000

    test(playwright): add axe playwright tests in bruno room

--- react-apps/bruno-room/playwright-tests/home.spec.ts
+++ react-apps/bruno-room/playwright-tests/home.spec.ts
@@ -1,7 +1,40 @@
+import AxeBuilder from "@axe-core/playwright";
 import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
   await page.goto("/");
 
   await expect(page).toHaveTitle("React App - Bruno Room | Threejs Journey");
+});
+
+test("has heading", async ({ browserName, page }) => {
+  // https://playwright.dev/docs/api/class-page#page-goto
+  test.fixme(
+    browserName === "firefox",
+    "Errors are occurring in firefox in CI pipeline",
+  );
+
+  await page.goto("/");
+
+  await expect(
+    page.getByRole("heading", {
+      name: "Bruno room",
+    }),
+  ).toBeVisible();
+});
+
+test("should not have any automatically detectable accessibility issues", async ({
+  browserName,
+  page,
+}) => {
+  // https://playwright.dev/docs/api/class-page#page-goto
+  test.fixme(
+    browserName === "firefox",
+    "Errors are occurring in firefox in CI pipeline",
+  );
+  await page.goto("/");
+
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+  expect(accessibilityScanResults.violations).toEqual([]);
 });
